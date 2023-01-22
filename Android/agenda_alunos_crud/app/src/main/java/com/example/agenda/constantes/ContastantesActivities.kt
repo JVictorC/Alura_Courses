6e4e41f742719165188536115bde0630921d0275
Joao Vitor Cordeiro <joao.csilva@spread.com.br>
2023-01-22 13:03:16 -0300

    Terminando Primeiro Curso Android

---
+++ Android/agenda_alunos_crud/app/src/main/java/com/example/agenda/constantes/ContastantesActivities.kt
@@ -0,0 +1,4 @@
+package com.example.agenda.constantes
+
+interface ContastantesActivities {
+}
---
+++ Android/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.agenda.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioALunoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_aluno)
+    }
+}
