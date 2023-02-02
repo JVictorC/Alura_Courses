a43dd29aa2a23dbb4c804166214ff9994210574b
Joao Vitor Cordeiro <joao.csilva@spread.com.br>
2023-02-02 08:42:01 -0300

    Atualizando Remoto

--- Android/agenda_alunos_parte_dois/app/src/main/java/com/example/agenda/constantes/ContastantesActivities.kt
+++
@@ -1,7 +0,0 @@
-package com.example.agenda.constantes
-
-interface ContastantesActivities {
-    companion object {
-        public var CHAVE_ALUNO: String = "aluno"
-    }
-}
--- Android/agenda_alunos_parte_tres/app/src/main/java/com/example/agenda/constantes/ContastantesActivities.kt
+++
@@ -1,7 +0,0 @@
-package com.example.agenda.constantes
-
-interface ContastantesActivities {
-    companion object {
-        var CHAVE_ALUNO: String = "aluno"
-    }
-}
